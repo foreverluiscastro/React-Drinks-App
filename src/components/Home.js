5964537c0f6e60f86d68e40d84f6bc11977cacf4
luis <foreverluiscastro@gmail.com>
2021-05-04 13:56:05 -0400

    added text to the home.js component

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,13 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>This is my Home Page!</h1>
+            <h1>The Beer Fridge!</h1>
+            <p><i>But Luis, what <b>is</b> The Beer Fridge?!?!</i></p>
+            <p>Rejoice, Beer Afcionados, The archaic time of writng down lists of beers using crayons and old CVS receipts are over</p>
+            <p>No longer will our phones' notes app be cluttered with the memories of beer run past</p>
+            <p>The Beer Fridge allows you to browse from a selection of <b>Beers</b> and add your favorites to <b>The Fridge!</b></p>
+            <p>In <b>The Fridge</b>, you will be able to access your favorited beers anytime you want!</p>
+
         </div>
     );
 };
