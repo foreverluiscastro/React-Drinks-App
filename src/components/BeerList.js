6b587abce3c640b119b8738fa181627f26407aa4
luis <foreverluiscastro@gmail.com>
2021-05-04 02:29:33 -0400

    added a BeerList component that renders beers

---
+++ src/components/BeerList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const BeerList = ({ beers }) => {
+    const renderBeers = Object.keys(beers).map(beerID =>
+        <Link key={beerID} to={`/beers/${beerID}`}>{beers[beerID].title}</Link>
+    );
+    
+    return (
+        <div>
+            {renderBeers}
+        </div>
+    )
+}
+
+export default BeerList;
