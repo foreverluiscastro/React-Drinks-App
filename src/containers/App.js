2212796525df20bbd441126392440356ff05a863
luis <foreverluiscastro@gmail.com>
2021-05-04 02:33:25 -0400

    moved app.js to containers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
+import NavBar from '../components/NavBar';
 
 class App extends Component {
 
