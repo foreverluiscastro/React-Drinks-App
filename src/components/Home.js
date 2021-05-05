e70f4080183ecd0823bd8bc20ee61145b9d8a8b6
luis <foreverluiscastro@gmail.com>
2021-05-05 00:31:43 -0400

    Updated the Home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,10 @@ const Home = () => {
         <div>
             <h1>Welcome to The Beer Fridge!</h1>
             <p><i>But Luis, what <b>is</b> The Beer Fridge?!?!</i></p>
-            <p>Rejoice, Beer Afcionados, The archaic time of writng down lists of beers using crayons and old CVS receipts are over</p>
-            <p>No longer will our phones' notes app be cluttered with the memories of beer run past</p>
-            <p>The Beer Fridge allows you to browse from a selection of <b>Beers</b> and add your favorites to the <b>Fridge</b>!</p>
-            <p>In the <b>Fridge</b>, you will be able to access your favorited beers anytime you want!</p>
+            <p>Rejoice, Beer Aficionados, The archaic time of writng down lists of beers and then shoving those lists into your pockets are <b>OVER!</b></p>
+            <p>No longer will our phones' notes app be cluttered with the memories of beer run past!</p>
+            <p>The Beer Fridge allows you to browse from a selection of <b>Beers</b> and let's you add your favorites to the <b>Fridge</b>!</p>
+            <p>In the <b>Fridge</b>, you will be able to access your favorited beers and update them anytime you want!</p>
 
         </div>
     );
