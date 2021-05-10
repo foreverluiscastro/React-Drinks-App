8c2d1ff8703706c9f90c5fc27d793c32391f1b38
luis <foreverluiscastro@gmail.com>
2021-05-10 16:42:17 -0400

    updated a few gramatical errors

--- src/components/SavedBeerShow.js
+++ src/components/SavedBeerShow.js
@@ -13,7 +13,8 @@ const SavedBeerShow = ({match, savedBeers, deleteBeer}) => {
             <h3>{ beer.name }</h3>
             <img src={ beer.image_url } class="beer-avatar" alt=""/>
             <p><b>Tagline:</b><i>{ beer.tagline }</i></p>
-            <p><b>Abv:</b>{ beer.description }</p>
+            <p><b>Abv:</b>{ beer.abv }</p>
+            <p><b>Description:</b>{ beer.description }</p>
             <p><b>Pairs well with:</b>{ beer.food_pairing.join(", ") }</p>
             <button onClick={handleClick}>Remove Beer From Fridge</button>
         </div>
